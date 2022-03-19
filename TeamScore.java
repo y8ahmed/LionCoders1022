d73ebf728f41dec48b0340e78ead1bc2226ebc3d
XuLongJiang <92057618+XuLongJiang@users.noreply.github.com>
2022-03-19 13:18:29 -0400

    Add files via upload

---
+++ TeamScore.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TeamScore {
+
+    public static boolean createWinner(int points1, int points2)
+    {
+        if (points1 > points2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int[] t1Array = new int[4];
+        int[] t2Array = new int[4];
+
+        int team1Score = 0;
+        int team2Score = 0;
+
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Enter Team 1 points: ");
+            t1Array[i] = Integer.parseInt(in.nextLine());
+            System.out.println("Enter Team 2 points: ");
+            t2Array[i] = Integer.parseInt(in.nextLine());
+        }
+    }
+}
